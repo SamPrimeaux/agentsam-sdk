21c0771fa62197150fbafaa8a295bafb4ce3f3bd
sam primeaux <inneranimalclothing@gmail.com>
2026-07-02 00:25:26 -0500

    export security sca module

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ export { AgentSam } from './AgentSam.js';
 export { routeIntent } from './lib/router.js';
 export { getToolCatalog } from './lib/tools.js';
 export { scaffoldProject } from './lib/scaffold.js';
+export {
+  collectNpmDependencies,
+  createOsvQueries,
+  findSecurityManifests,
+  formatSecurityReport,
+  queryOsv,
+  scanProjectSecurity,
+  summarizeVulnerabilities,
+} from './security/sca.js';
 
 export const version = pkg.version;
 export const name = pkg.name;
