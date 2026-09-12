6e076c1c092d9ffbc739d8aa640a857dab9cc75f
sam primeaux <inneranimalclothing@gmail.com>
2026-09-12 16:53:27 -0500

    refactor(repository): compose root repository facade from package

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,5 +1,5 @@
 export { repositorySnapshot } from '../capabilities/repository-snapshot.js';
-export { buildMerkleTree, diffTrees, readSnapshot, saveSnapshot, validateSnapshot } from '../lib/merkle/index.js';
+export { buildMerkleTree, diffTrees, readSnapshot, saveSnapshot, validateSnapshot } from '../../packages/agentsam-repository/src/merkle/index.js';
 export { resolveGitContext, tryResolveGitContext, normalizeGitRemote } from '../lib/git-context.js';
 export { buildRetrievalPlan, createContextPack } from '../knowledge/index.js';
 export {
