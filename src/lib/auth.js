5b97742f59928828aa251b5374e9a148a8d933a9
sam primeaux <inneranimalclothing@gmail.com>
2026-09-12 03:59:08 -0500

    feat: persist successful browser auth outside project state

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,6 +5,7 @@ import http from 'node:http';
 import { randomBytes } from 'node:crypto';
 import { postJson } from './core-client.js';
 import { promptToOpenUrl } from './open-url.js';
+import { saveAccountSession } from './account-session.js';
 
 function randomState() {
   return randomBytes(16).toString('hex');
