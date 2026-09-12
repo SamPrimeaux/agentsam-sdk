025032204f3dc568e0cb468a8dc24682c25e5a72
sam primeaux <inneranimalclothing@gmail.com>
2026-09-12 16:57:49 -0500

    refactor(repository): route deploy receipt policy through package

--- src/lib/deploy-receipt/index.js
+++ src/lib/deploy-receipt/index.js
@@ -3,7 +3,7 @@ import path from 'node:path';
 import { execFileSync } from 'node:child_process';
 import { randomBytes } from 'node:crypto';
 import { buildMerkleTree, diffTrees, readSnapshot, validateSnapshot } from '../../../packages/agentsam-repository/src/merkle/index.js';
-import { normalizePolicy } from '../merkle/policy.js';
+import { normalizePolicy } from '../../../packages/agentsam-repository/src/merkle/policy.js';
 
 export const DEFAULT_DEPLOY_EXCLUDES = Object.freeze([
   '.agentsam/deploy-merkle',
