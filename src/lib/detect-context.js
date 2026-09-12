e63ebce5d1f74620fe5933039b5a7899d137b037
sam primeaux <inneranimalclothing@gmail.com>
2026-09-12 04:00:49 -0500

    feat: let context detection resolve machine-local AgentSam identity

--- src/lib/detect-context.js
+++ src/lib/detect-context.js
@@ -5,7 +5,7 @@
 import { execFile } from 'child_process';
 import { promisify } from 'util';
 import { coreBaseUrl } from './core-client.js';
-import { resolveSdkKey } from '../../packages/identity/src/contracts/auth-config.js';
+import { resolveAccountSdkKey } from './account-session.js';
 
 const execFileAsync = promisify(execFile);
 
