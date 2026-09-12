3260ddabae662ac52afba46d0ce30391d3124c93
sam primeaux <inneranimalclothing@gmail.com>
2026-09-12 16:55:22 -0500

    refactor(repository): export graph runtime contracts from package

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ export {
   createRepositoryIdentity,
   createRepositoryContract,
   createRepositoryDependency,
-} from './repository/contracts.js';
+} from '../packages/agentsam-repository/src/contracts.js';
 export {
   DEFAULT_CONTEXT_RATIOS,
   DEFAULT_RESULT_POLICY,
