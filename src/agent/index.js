d3a1bea354a19fb979caedc396258b435eb29f2e
sam primeaux <inneranimalclothing@gmail.com>
2026-09-12 02:41:03 -0500

    feat: export bounded Responses runner

--- src/agent/index.js
+++ src/agent/index.js
@@ -1,2 +1,3 @@
 export { createCapabilityAdapter } from './capability-adapter.js';
 export { buildRepositoryAuditPacket, renderRepositoryAuditMarkdown, runRepositoryAudit } from './repository-audit.js';
+export { buildAgentToolSurface, capabilityFunctionName, runResponsesAgent } from './responses-runner.js';
