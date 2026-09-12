ff9d8184bfb661c996a10339084b99f4d3033a9d
sam primeaux <inneranimalclothing@gmail.com>
2026-09-12 16:54:14 -0500

    refactor(repository): use package Merkle in snapshot capability

--- src/capabilities/repository-snapshot.js
+++ src/capabilities/repository-snapshot.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'node:url';
 import { createHash } from 'node:crypto';
 import { resolveGitContext } from '../../packages/agentsam-repository/src/git-context.js';
 import { getRepositoryId, tryReadProjectConfig } from '../lib/project-config.js';
-import { buildMerkleTree } from '../lib/merkle/index.js';
+import { buildMerkleTree } from '../../packages/agentsam-repository/src/merkle/index.js';
 import { gitIgnoredPaths } from '../lib/merkle/git-ignore.js';
 import { analyzeExecutionBoundaries } from '../indexing/execution-boundary.js';
 import { showLatestDeployReceipt } from '../lib/deploy-receipt/index.js';
