48bb470e35745f13b6c7577e16bfd1715d28289a
sam primeaux <inneranimalclothing@gmail.com>
2026-09-17 20:13:22 -0500

    feat(storage): apply bundled migrations during db init

--- src/local/sqlite.js
+++ src/local/sqlite.js
@@ -78,6 +78,7 @@ export async function initializeLocalSqlite({
   const db = await createLocalSqliteDatabase(resolvedDb);
   try {
     db.exec(schema);
+    await applyRuntimeMigrations(db);
   } finally {
     db.close();
   }
