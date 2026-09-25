112cf2c230ee78ae0d3fb41daee3938425667066
sam primeaux <inneranimalclothing@gmail.com>
2026-09-25 04:05:30 -0500

    Pass Go deployment evidence into registry SQL

--- src/go/registry.js
+++ src/go/registry.js
@@ -158,6 +158,10 @@ export function applyGoProductRegistry({
     url,
     commit,
     health,
+    workerDeploymentId,
+    workerVersionId,
+    artifactDigest,
+    containerDigest,
     repositoryId,
   });
 
