82deb1cbae290ae963e08946d6fac8b4220de6ff
sam primeaux <inneranimalclothing@gmail.com>
2026-09-26 16:08:45 -0500

    refactor(database-editor): add portable frontend backend package split

---
+++ packages/agentsam-database-editor/backend/index.js
@@ -0,0 +1,11 @@
+export {
+  createD1Adapter,
+  createD1BindingAdapter,
+} from "../src/adapters/d1.js";
+export {
+  createPostgresAdapter,
+  createHyperdriveAdapter,
+} from "../src/adapters/postgres.js";
+export { createSupabaseAdapter } from "../src/adapters/supabase.js";
+export { readD1Metrics } from "../src/metrics/d1.js";
+export { readPostgresMetrics } from "../src/metrics/postgres.js";
