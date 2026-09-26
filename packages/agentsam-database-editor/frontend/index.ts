c275ef0aa392035459dbde58b86b8e85c4a5c3a2
sam primeaux <inneranimalclothing@gmail.com>
2026-09-26 16:08:43 -0500

    refactor(database-editor): add portable frontend backend package split

---
+++ packages/agentsam-database-editor/frontend/index.ts
@@ -0,0 +1,12 @@
+export { DatabaseEditorApp } from "../src/ui/DatabaseEditorApp";
+export type { DatabaseEditorAppProps } from "../src/ui/DatabaseEditorApp";
+export { createDatabaseStudioClient } from "../src/ui/client";
+export type {
+  DatabaseStudioClient,
+  DatabaseSource,
+  DatabaseMetricsResponse,
+  DatabaseTableInfo,
+  DatabaseSchemaInfo,
+  DatabaseRowsResponse,
+  DatabaseQueryResponse,
+} from "../src/ui/client";
