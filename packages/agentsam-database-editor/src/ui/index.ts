d06aec319fc01f71e5a3ebe6ba0a9b07d8cb33b7
sam primeaux <inneranimalclothing@gmail.com>
2026-09-26 16:03:55 -0500

    feat(database-editor): add UI barrel export

---
+++ packages/agentsam-database-editor/src/ui/index.ts
@@ -0,0 +1,12 @@
+export { DatabaseEditorApp } from "./DatabaseEditorApp";
+export type { DatabaseEditorAppProps } from "./DatabaseEditorApp";
+export { createDatabaseStudioClient } from "./client";
+export type {
+  DatabaseStudioClient,
+  DatabaseSource,
+  DatabaseMetricsResponse,
+  DatabaseTableInfo,
+  DatabaseSchemaInfo,
+  DatabaseRowsResponse,
+  DatabaseQueryResponse,
+} from "./client";
